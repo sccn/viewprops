98bb92f03f7222865838a46308233f4ce23c8d21
LucaPT <lucapton@yahoo.com>
2017-08-31 18:15:58 -0700

    fixed more bugs related to saving EEG history

--- eegplugin_viewprops.m
+++ eegplugin_viewprops.m
@@ -1,6 +1,6 @@
 function eegplugin_viewprops( fig, try_strings, catch_strings )
-%EEGLABPLUGIN_POP_PROP_EXTENDED Summary of this function goes here
-%   Detailed explanation goes here
+%EEGLABPLUGIN_POP_PROP_EXTENDED View extended channel/component properties
+%   Displays ERPimage, spectopo, topomap, activity scroll, dipole, and pvaf
 vers = '1.04';
 if nargin < 3
     error('eegplugin_viewprops requires 3 arguments');
@@ -8,8 +8,8 @@ end
 
 plotmenu = findobj(fig, 'tag', 'plot');
 uimenu( plotmenu, 'label', 'View extended channel properties', ...
-    'callback', [try_strings.no_check '[temp1, temp2, LASTCOM] = pop_viewprops(EEG, 1);' catch_strings.add_to_hist]);
+    'callback', [try_strings.no_check 'LASTCOM = pop_viewprops(EEG, 1);' catch_strings.add_to_hist]);
 uimenu( plotmenu, 'label', 'View extended component properties', ...
-    'callback', [try_strings.no_check '[temp1, temp2, LASTCOM] = pop_viewprops(EEG, 0);' catch_strings.add_to_hist]);
+    'callback', [try_strings.no_check 'LASTCOM = pop_viewprops(EEG, 0);' catch_strings.add_to_hist]);
 end
 
